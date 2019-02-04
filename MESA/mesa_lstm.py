6b6e6ab406ba952d54fb116a26571e1fa7496c48
yiorg <yiorgos.christakis@pfizer.com>
2019-02-04 13:33:02 -0500

    updates for hyper param tuning

--- MESA/mesa_lstm.py
+++ MESA/mesa_lstm.py
@@ -6,28 +6,11 @@ from mesa_utils import Generator
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
-
-# SETUP
-data_dir = 'mesa-commercial-use/synced/'
-file_list = sorted(filter(lambda x: '.csv' in x, os.listdir(data_dir)))
-params = {'look back': 20,
-          'step': 1,
-          'delay': -10,
-          'batch size': 128,
-          'input columns': 1,
-          'number of training subjects': 1300,
-          'number of testing subjects': 300,
-          'epochs': 10,
-          'nodes': 64,
-          'steps per epoch': 100,
-          'enable plots': False,
-          'optimizer': 'adam',
-          'loss': 'mse',
-          'metrics': ['accuracy']}
+import random
+random.seed(1)
 
 
-
-def run(data_dir, file_list, params):
+def run(data_dir, file_list, params, save=False):
     # params
     look_back = params['look back']     # how many steps to look back from current point
     step = params['step']       # number of steps between samples
@@ -39,14 +22,11 @@ def run(data_dir, file_list, params):
     epochs = params['epochs']       # number of epochs
     nodes = params['nodes']     # number of nodes in the network
     steps_per_epoch = params['steps per epoch']     # number of steps per epoch
-    enable_plots = params['enable plots']       # plot?
     optimizer = params['optimizer']     # kind of optimizer
     loss = params['loss']       # loss function
     metrics = params['metrics']     # metrics to be recorded while training/testing
 
-
     # MODEL DEFINITION
-    print "Defining model..."
     model = Sequential()
     model.add(LSTM(nodes, input_shape=(look_back // step, input_cols)))
     model.add(Dense(1))
@@ -54,30 +34,28 @@ def run(data_dir, file_list, params):
                   loss=loss,
                   metrics=metrics)
 
-
     # RUN
     metrics_list = []
-    history = []
     for i, f in enumerate(file_list):
 
         # RUN TRAINING SUBJECTS
-        if i < num_train_subs:
+        if i <= num_train_subs:
             f_name = os.path.join(data_dir, f)
             gen = Generator(f_name, source='MESA')
             train_gen = gen.flow(look_back=look_back,
                                  delay=delay,
                                  min_index=0,
+                                 max_index=None,
                                  step=step,
                                  batch_size=batch_size)
 
-            print "Running model fit for subject {}...".format(f_name.split('-')[-1][:4])
-            history = model.fit_generator(train_gen,
-                                          steps_per_epoch=steps_per_epoch,
-                                          epochs=epochs,
-                                          verbose=2)
+            model.fit_generator(train_gen,
+                                steps_per_epoch=steps_per_epoch,
+                                epochs=epochs,
+                                verbose=0)
 
         # RUN TESTING SUBJECTS
-        elif i < num_train_subs + num_test_subs:
+        elif i <= num_train_subs + num_test_subs:
             f_name = os.path.join(data_dir, f)
             gen = Generator(f_name, source='MESA')
             test_gen = gen.flow(look_back=look_back,
@@ -87,9 +65,7 @@ def run(data_dir, file_list, params):
                                 step=step,
                                 batch_size=batch_size)
 
-            print "Running evaluator for subject {}...".format(f_name.split('-')[-1][:4])
             metrics_list.append(model.evaluate_generator(test_gen, len(gen.data)-1-look_back, verbose=False))
-            print "Loss and accuracy: ", metrics_list[-1]
 
         # FINISH
         else:
@@ -100,15 +76,50 @@ def run(data_dir, file_list, params):
     average_metrics = metrics_list.mean(axis=0)
     acc = average_metrics[1]
     loss = average_metrics[0]
-    curr_time = str(datetime.datetime.now())[:-7]
-    print "Average accuracy: {}".format(acc)
-    print "Average loss: {}".format(loss)
+    if save:
+        curr_time = str(datetime.datetime.now())[:-7]
+        # save configuration
+        os.mkdir(curr_time)
+        model.save("{}/model.h5".format(curr_time))
+        with open('{}/params.txt'.format(curr_time), 'w+') as f:
+            f.write(json.dumps(params))
+        with open('{}/results.txt'.format(curr_time), 'w+') as f:
+            f.write("Average accuracy: {}\n".format(acc))
+            f.write("Average loss: {}\n".format(loss))
+    return acc, loss
 
-    # save configuration
+
+if __name__ == '__main__':
+
+    # SETUP
+    data = 'mesa-commercial-use/synced/'
+    files = sorted(filter(lambda x: '.csv' in x, os.listdir(data)))
+    random.shuffle(files)
+    parameters = {'look back': 20,
+                  'step': 1,
+                  'delay': -10,
+                  'batch size': 128,
+                  'input columns': 1,
+                  'number of training subjects': 130,
+                  'number of testing subjects': 30,
+                  'epochs': 20,
+                  'nodes': 10,
+                  'steps per epoch': 100,
+                  'optimizer': 'adam',
+                  'loss': 'mse',
+                  'metrics': ['accuracy']}
+
+    res = []
+    for lb in range(10, 26, 2):
+        for nd in range(8, 100, 8):
+            parameters['nodes'] = nd
+            parameters['look back'] = lb
+            parameters['delay'] = -1 * lb/2
+            ac, los = run(data, files, parameters)
+            res.append([ac, los, ('look back', lb), ('delay', lb/2), ('nodes', nd)])
+    # save results
+    curr_time = str(datetime.datetime.now())[:-7]
     os.mkdir(curr_time)
-    model.save("{}/model.h5".format(curr_time))
-    with open('{}/params.txt'.format(curr_time), 'w+') as f:
-        f.write(json.dumps(params))
     with open('{}/results.txt'.format(curr_time), 'w+') as f:
-        f.write("Average accuracy: {}\n".format(acc))
-        f.write("Average loss: {}\n".format(loss))
+        for line in res:
+            f.write(str(line)[1:-1] + "\n")
